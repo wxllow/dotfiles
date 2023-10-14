f3f8cb1ecd7b74c08135c5e93d8aa82f65e82a0a
wxllow <willow@wxllow.dev>
2023-10-14 15:25:13 -0400

    Update

--- hyprland/scripts/suspend.sh
+++ hyprland/scripts/suspend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 swayidle -w \
-timeout 120 ' gtklock ' \
-timeout 600 ' hyprctl dispatch dpms off' \
-resume ' hyprctl dispatch dpms on' \
-before-sleep ' gtklock '
+timeout 120 'gtklock -d' \
+timeout 600 'hyprctl dispatch dpms off' \
+resume 'hyprctl dispatch dpms on' \
+after-resume 'hyprctl dispatch dpms on' \
+lock 'gtklock -d'
