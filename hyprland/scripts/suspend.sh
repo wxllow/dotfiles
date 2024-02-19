7772eb0264a1abbfa9940909db0a31cf9370c7ea
wxllow <willow@wxllow.dev>
2024-02-19 13:31:43 -0500

    Upd

--- hyprland/scripts/suspend.sh
+++ hyprland/scripts/suspend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 swayidle -w \
-    timeout 120 'swaylock -d' \
+    timeout 180 'swaylock -d --grace 10' \
     timeout 600 'hyprctl dispatch dpms off' \
     timeout 600 '~/.config/hypr/scripts/sleep.sh' \
     resume 'hyprctl dispatch dpms on' \
